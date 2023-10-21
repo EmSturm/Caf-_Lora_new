03b619a474746038881e81b20b213aa9bfcb7c3b
EmSturm <emsturmova@gmail.com>
2023-10-21 15:41:07 +0200

    Komponenty a posluchač na menu

---
+++ src/components/Drink/index.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+
+export const Drink = ({id, name, ordered, image, layers }) => {
+    return (
+        <div className="drink">
+            <div className="drink__product">
+              <div className="drink__cup">
+                <img src={image} />
+              </div>
+              <div className="drink__info">
+                <h3>{name}</h3>
+                <div className="layer">
+                  <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
+                  <div className="layer__label">espresso</div>
+                </div>
+              </div>
+            </div>
+            <div className="drink__controls">
+                <form action="">
+                    <button className="order-btn" type="submit">Objednat</button>
+                    <input type="hidden" value={id} />
+                </form>
+              <button className="order-btn">Objednat</button>
+            </div>
+          </div>
+
+    )
+}
